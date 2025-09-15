55f19c47c1039f5c6ec6fc2dec868f32516ca7a2
Sinxay Souvannavong <sinxay.souvannavong@massoutre-locations.com>
2025-09-15 11:08:07 +0200

    sava facture uniquement si c'est une facture de vehicule selling

--- RecupKepler_Factures.php
+++ RecupKepler_Factures.php
@@ -77,6 +77,11 @@
             foreach ($obj_final as $keydatas => $keyvalue) {
                 //on boucle par rapport au nombre de bon de commande dans le tableau datas[]
     
+                 // enregistrer la facture dans la base > table : portail_massoutre > facturesventes si c'est une facture VOxxxxx , si c'est une FAPxxx ne pas save
+                if (strpos($keyvalue->number, "FAP") === false) {
+                    save_facture_to_portail_massoutre($keyvalue);
+                }
+
                 //get date facture
                 $date_facture_tmp = substr($keyvalue->invoiceDate, 0, 10);
                 $date_facture_tmp2 = str_replace("-", "/", $date_facture_tmp);
@@ -179,9 +184,6 @@
                 if (isset($keyvalue->items)) {
                     foreach ($keyvalue->items as $key_item => $value_item) {
                         if ($value_item->type == 'vehicle_selling') {
-
-                            // enregistrer la facture dans la base > table : portail_massoutre > facturesventes
-                            save_facture_to_portail_massoutre($keyvalue);
 
                             $reference_item = $value_item->reference;
 
